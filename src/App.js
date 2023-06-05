4b82e74e2a28c720f679156bc9ec9da82a1b0cb5
aasrith-dondapati <aasrith.au@gmail.com>
2023-06-05 12:18:35 +0930

    sequence code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const KeyboardLayout = () => {
+  const keyboard = [
+    ['A', 'B', 'C', 'D', 'E'],
+    ['F', 'G', 'H', 'I', 'J'],
+    ['K', 'L', 'M', 'N', 'O'],
+    ['1', '2', '3']
+  ];
+
+  const knightMoves = [
+    [-2, -1], [-2, 1],
+    [-1, -2], [-1, 2],
+    [1, -2],  [1, 2],
+    [2, -1],  [2, 1]
+  ];
+
+  const [totalSequences, setTotalSequences] = useState(0);
+
+  useEffect(() => {
+    const countSequences = () => {
+      let count = 0;
+
+      const dfs = (row, col, vowelsCount, sequenceLength) => {
+        const Vowel = keyboard[row][col].match(/[AEIOU]/i);
+
+        if (Vowel && vowelsCount >= 2) {
+          return;
+        }
+
+        if (sequenceLength === 10) {
+          count++;
+          return;
+        }
+
+        for (const move of knightMoves) {
+          const nextRow = row + move[0];
+          const nextCol = col + move[1];
+
+          if (
+            nextRow >= 0 &&
+            nextRow < keyboard.length &&
+            nextCol >= 0 &&
+            nextCol < keyboard[nextRow].length
+          ) {
+            dfs(nextRow, nextCol, vowelsCount + (Vowel ? 1 : 0), sequenceLength + 1);
+          }
+        }
+      };
+
+      for (let row = 0; row < keyboard.length; row++) {
+        for (let col = 0; col < keyboard[row].length; col++) {
+          dfs(row, col, 0, 1);
+        }
+      }
+
+      return count;
+    };
+
+    const sequencesCount = countSequences();
+    setTotalSequences(sequencesCount);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign:'center', marginTop:'22.5%'}}>Total number of valid 10-key sequences: {totalSequences}</h1>
     </div>
   );
-}
+};
 
-export default App;
+export default KeyboardLayout;
